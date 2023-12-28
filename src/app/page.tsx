50f23f31bff1cf582e442ad3a93a09d31ffcba56
subhash131 <subhashnayak131@gmail.com>
2023-12-29 00:12:57 +0530

    test deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Footer from "@/components/Footer";
 import Pricing from "@/components/pricing/Pricing";
 import TechStack from "@/components/techStack/TechStack";
 import Testimonial from "@/components/testimonials/Testimonial";
-import Hero from "@/components/hero/Hero";
+// import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <div className="bg-[#0a0c11] w-full min-h-screen text-white flex flex-col gap-12 items-center">
       <Navbar />
-      <Hero />
+      {/* <Hero /> */}
       <TechStack />
       <Pricing />
       <Testimonial />
