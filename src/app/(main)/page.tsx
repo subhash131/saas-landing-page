941326f27df1b882b40308b7af180f757da1dd0e
subhash131 <subhashnayak131@gmail.com>
2023-12-29 00:02:53 +0530

    build fix: Hero import

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Hero from "@/components/hero/Hero";
 import Pricing from "@/components/pricing/Pricing";
 import TechStack from "@/components/techStack/TechStack";
 import Testimonial from "@/components/testimonials/Testimonial";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
